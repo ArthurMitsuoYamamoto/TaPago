d1ef10d0a99d596e0a541f0a43a0f4a35631bccd
WINDOWS <arthuryamamoto0@gmail.com>
2024-03-04 12:03:01 -0300

    feature: add post and get

---
+++ src/main/java/br/com/fiap/tapago/model/Categoria.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tapago.model;
+
+import java.util.Random;
+
+public record Categoria(Long id, String nome, String icone){
+    public Categoria(Long id, String nome, String icone){
+        this.id  = Math.abs (new Random().nextLong());
+        this.nome = nome;
+        this.icone = icone;
+    }
+}
+    
+//imutável - record
